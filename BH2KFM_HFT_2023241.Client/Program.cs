634af0733df224a3c4880f097bd1a348a4f666de
CsongorTiboldi <tiboldi.csongor@gmail.com>
2023-11-04 21:49:16 +0100

    ConsoleMenu configured for viewing stored data

--- BH2KFM_HFT_2023241.Client/Program.cs
+++ BH2KFM_HFT_2023241.Client/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using BH2KFM_HFT_2023241.Logic;
 using BH2KFM_HFT_2023241.Repository;
 using BH2KFM_HFT_2023241.Models;
+using ConsoleTools;
 
 namespace BH2KFM_HFT_2023241.Client
 {
@@ -9,7 +11,48 @@ namespace BH2KFM_HFT_2023241.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            LectureDbContext ctx = new LectureDbContext();
+            IRepository<Subject> subjectRepo= new SubjectRepository(ctx);
+            IRepository<Room> roomRepo = new RoomRepository(ctx);
+            IRepository<Course> courseRepo = new CourseRepository(ctx);
+
+            ISubjectLogic subjectLogic = new SubjectLogic(subjectRepo);
+            IRoomLogic roomLogic = new RoomLogic(roomRepo);
+            ICourseLogic courseLogic = new CourseLogic(courseRepo);
+
+
+            var subjectSubmenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () =>
+                {
+                    subjectLogic.ReadAll().ToList().ForEach(t => Console.WriteLine(t));
+                    Console.ReadLine();
+                })
+                .Add("Exit", ConsoleMenu.Close);
+
+            var roomSubmenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () =>
+                {
+                    roomLogic.ReadAll().ToList().ForEach(t => Console.WriteLine(t));
+                    Console.ReadLine();
+                })
+                .Add("Exit", ConsoleMenu.Close);
+
+            var courseSubmenu = new ConsoleMenu(args, level:1)
+                .Add("List", () =>
+                {
+                    courseLogic.ReadAll().ToList().ForEach(t => Console.WriteLine(t));
+                    Console.ReadLine();
+                })
+                .Add("Exit", ConsoleMenu.Close);
+
+            var mainMenu = new ConsoleMenu(args, level: 0)
+                .Add("Subjects",() => subjectSubmenu.Show())
+                .Add("Rooms",() => roomSubmenu.Show())
+                .Add("Courses",() => courseSubmenu.Show())
+                .Add("Exit", ConsoleMenu.Close);
+
+            mainMenu.Show();
         }
     }
 }
