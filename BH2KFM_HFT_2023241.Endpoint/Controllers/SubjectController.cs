407fa00c7f9debf3a19db816c559e026c3fa15b0
CsongorTiboldi <tiboldi.csongor@gmail.com>
2023-11-27 22:27:55 +0100

    Created endpoint controllers for SubjectLogic

---
+++ BH2KFM_HFT_2023241.Endpoint/Controllers/SubjectController.cs
@@ -0,0 +1,49 @@
+﻿using BH2KFM_HFT_2023241.Logic;
+using BH2KFM_HFT_2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BH2KFM_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SubjectController : ControllerBase
+    {
+        ISubjectLogic logic;
+
+        public SubjectController(ISubjectLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Subject> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Subject Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Subject value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] Subject value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
---
+++ BH2KFM_HFT_2023241.Endpoint/Controllers/SubjectStatController.cs
@@ -0,0 +1,49 @@
+﻿using BH2KFM_HFT_2023241.Logic;
+using BH2KFM_HFT_2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BH2KFM_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class SubjectStatController : ControllerBase
+    {
+        ISubjectLogic logic;
+
+        public SubjectStatController(ISubjectLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public double AverageCreditValue()
+        {
+            return this.logic.AverageCreditValue();
+        }
+
+        [HttpGet("{semester}")]
+        public IEnumerable<Subject> SubjectsInSemester(int semester)
+        {
+            return this.logic.SubjectsInSemester(semester);
+        }
+
+        [HttpGet("{credit}")]
+        public IEnumerable<Subject> SubjectsWithCreditValue(int credit)
+        {
+            return this.logic.SubjectsWithCreditValue(credit);
+        }
+
+        [HttpGet("{subject}")]
+        public IEnumerable<Room> Rooms(int subject)
+        {
+            return this.logic.Rooms(subject);
+        }
+
+        [HttpGet]
+        public int MostCreditSemester()
+        {
+            return this.logic.MostCreditSemester();
+        }
+    }
+}
