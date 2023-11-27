e036d69e0ae6281b451d0b8a6a44b75592117cf1
CsongorTiboldi <tiboldi.csongor@gmail.com>
2023-11-27 22:17:52 +0100

    Created endpoint controllers for RoomLogic

---
+++ BH2KFM_HFT_2023241.Endpoint/Controllers/RoomController.cs
@@ -0,0 +1,49 @@
+﻿using BH2KFM_HFT_2023241.Logic;
+using BH2KFM_HFT_2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BH2KFM_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        IRoomLogic logic;
+
+        public RoomController(IRoomLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Room> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Room Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Room value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Update([FromBody] Room value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
---
+++ BH2KFM_HFT_2023241.Endpoint/Controllers/RoomStatController.cs
@@ -0,0 +1,49 @@
+﻿using BH2KFM_HFT_2023241.Logic;
+using BH2KFM_HFT_2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BH2KFM_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class RoomStatController : ControllerBase
+    {
+        IRoomLogic logic;
+
+        public RoomStatController(IRoomLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Room> ProjectorRooms()
+        {
+            return this.logic.ProjectorRooms();
+        }
+
+        [HttpGet]
+        public int MaxCapacity()
+        {
+            return this.logic.MaxCapacity();
+        }
+
+        [HttpGet]
+        public IEnumerable<Room> LargestCapacityRooms()
+        {
+            return this.logic.LargestCapacityRooms();
+        }
+
+        [HttpGet]
+        public double AverageCapacity()
+        {
+            return this.logic.AverageCapacity();
+        }
+
+        [HttpGet("{roomId}")]
+        public IEnumerable<Subject> Subjects(int roomId)
+        {
+            return this.logic.Subjects(roomId);
+        }
+    }
+}
