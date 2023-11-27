c938448d446990d9a52e0bda40e14b9ba7b0de99
CsongorTiboldi <tiboldi.csongor@gmail.com>
2023-11-27 23:20:50 +0100

    Added exception handling to Endpoint

--- BH2KFM_HFT_2023241.Endpoint/Startup.cs
+++ BH2KFM_HFT_2023241.Endpoint/Startup.cs
@@ -2,7 +2,9 @@ using BH2KFM_HFT_2023241.Logic;
 using BH2KFM_HFT_2023241.Models;
 using BH2KFM_HFT_2023241.Repository;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -55,6 +57,15 @@ namespace BH2KFM_HFT_2023241.Endpoint
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BH2KFM_HFT_2023241.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                .Get<IExceptionHandlerPathFeature>()
+                .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
 
             app.UseRouting();
 
