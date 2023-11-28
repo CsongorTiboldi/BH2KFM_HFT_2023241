72757b28d909565a7bc382c8ea3f2a93a0585d53
CsongorTiboldi <tiboldi.csongor@gmail.com>
2023-11-28 20:58:43 +0100

    Client prepared for api based operation

--- BH2KFM_HFT_2023241.Client/Program.cs
+++ BH2KFM_HFT_2023241.Client/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Linq;
-using BH2KFM_HFT_2023241.Logic;
-using BH2KFM_HFT_2023241.Repository;
 using BH2KFM_HFT_2023241.Models;
 using ConsoleTools;
 
@@ -9,50 +7,43 @@ namespace BH2KFM_HFT_2023241.Client
 {
     internal class Program
     {
+        static RestService rest;
+
         static void Main(string[] args)
         {
+            rest = new RestService("http://localhost:60321/");
 
-            LectureDbContext ctx = new LectureDbContext();
-            IRepository<Subject> subjectRepo= new SubjectRepository(ctx);
-            IRepository<Room> roomRepo = new RoomRepository(ctx);
-            IRepository<Course> courseRepo = new CourseRepository(ctx);
+            //var subjectSubmenu = new ConsoleMenu(args, level: 1)
+            //    .Add("List", () =>
+            //    {
+            //        subjectLogic.ReadAll().ToList().WriteToConsole();
+            //        Console.ReadLine();
+            //    })
+            //    .Add("Exit", ConsoleMenu.Close);
 
-            ISubjectLogic subjectLogic = new SubjectLogic(subjectRepo);
-            IRoomLogic roomLogic = new RoomLogic(roomRepo);
-            ICourseLogic courseLogic = new CourseLogic(courseRepo);
+            //var roomSubmenu = new ConsoleMenu(args, level: 1)
+            //    .Add("List", () =>
+            //    {
+            //        roomLogic.ReadAll().ToList().WriteToConsole();
+            //        Console.ReadLine();
+            //    })
+            //    .Add("Exit", ConsoleMenu.Close);
 
+            //var courseSubmenu = new ConsoleMenu(args, level:1)
+            //    .Add("List", () =>
+            //    {
+            //        courseLogic.ReadAll().ToList().WriteToConsole();
+            //        Console.ReadLine();
+            //    })
+            //    .Add("Exit", ConsoleMenu.Close);
 
-            var subjectSubmenu = new ConsoleMenu(args, level: 1)
-                .Add("List", () =>
-                {
-                    subjectLogic.ReadAll().ToList().WriteToConsole();
-                    Console.ReadLine();
-                })
-                .Add("Exit", ConsoleMenu.Close);
+            //var mainMenu = new ConsoleMenu(args, level: 0)
+            //    .Add("Subjects",() => subjectSubmenu.Show())
+            //    .Add("Rooms",() => roomSubmenu.Show())
+            //    .Add("Courses",() => courseSubmenu.Show())
+            //    .Add("Exit", ConsoleMenu.Close);
 
-            var roomSubmenu = new ConsoleMenu(args, level: 1)
-                .Add("List", () =>
-                {
-                    roomLogic.ReadAll().ToList().WriteToConsole();
-                    Console.ReadLine();
-                })
-                .Add("Exit", ConsoleMenu.Close);
-
-            var courseSubmenu = new ConsoleMenu(args, level:1)
-                .Add("List", () =>
-                {
-                    courseLogic.ReadAll().ToList().WriteToConsole();
-                    Console.ReadLine();
-                })
-                .Add("Exit", ConsoleMenu.Close);
-
-            var mainMenu = new ConsoleMenu(args, level: 0)
-                .Add("Subjects",() => subjectSubmenu.Show())
-                .Add("Rooms",() => roomSubmenu.Show())
-                .Add("Courses",() => courseSubmenu.Show())
-                .Add("Exit", ConsoleMenu.Close);
-
-            mainMenu.Show();
+            //mainMenu.Show();
         }
     }
 }
