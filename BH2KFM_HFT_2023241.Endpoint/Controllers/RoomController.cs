88eab368bacdf7e2149fedf1398130a5e5423620
CsongorTiboldi <tiboldi.csongor@gmail.com>
2024-05-06 15:28:16 +0200

    Configured SignalR on endpoint controllers

--- BH2KFM_HFT_2023241.Endpoint/Controllers/RoomController.cs
+++ BH2KFM_HFT_2023241.Endpoint/Controllers/RoomController.cs
@@ -1,6 +1,8 @@
-﻿using BH2KFM_HFT_2023241.Logic;
+﻿using BH2KFM_HFT_2023241.Endpoint.Services;
+using BH2KFM_HFT_2023241.Logic;
 using BH2KFM_HFT_2023241.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System.Collections.Generic;
 
 namespace BH2KFM_HFT_2023241.Endpoint.Controllers
@@ -10,10 +12,12 @@ namespace BH2KFM_HFT_2023241.Endpoint.Controllers
     public class RoomController : ControllerBase
     {
         IRoomLogic logic;
+        IHubContext<SignalRHub> hub;
 
-        public RoomController(IRoomLogic logic)
+        public RoomController(IRoomLogic logic, IHubContext<SignalRHub> hub)
         {
             this.logic = logic;
+            this.hub = hub;
         }
 
         [HttpGet]
@@ -32,18 +36,22 @@ namespace BH2KFM_HFT_2023241.Endpoint.Controllers
         public void Create([FromBody] Room value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("RoomCreated", value);
         }
 
         [HttpPut]
         public void Update([FromBody] Room value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("RoomUpdated", value);
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var value = this.logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("RoomDeleted", value);
         }
     }
 }
--- BH2KFM_HFT_2023241.Endpoint/Controllers/SubjectController.cs
+++ BH2KFM_HFT_2023241.Endpoint/Controllers/SubjectController.cs
@@ -1,6 +1,8 @@
-﻿using BH2KFM_HFT_2023241.Logic;
+﻿using BH2KFM_HFT_2023241.Endpoint.Services;
+using BH2KFM_HFT_2023241.Logic;
 using BH2KFM_HFT_2023241.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System.Collections.Generic;
 
 namespace BH2KFM_HFT_2023241.Endpoint.Controllers
@@ -10,10 +12,12 @@ namespace BH2KFM_HFT_2023241.Endpoint.Controllers
     public class SubjectController : ControllerBase
     {
         ISubjectLogic logic;
+        IHubContext<SignalRHub> hub;
 
-        public SubjectController(ISubjectLogic logic)
+        public SubjectController(ISubjectLogic logic, IHubContext<SignalRHub> hub)
         {
             this.logic = logic;
+            this.hub = hub;
         }
 
         [HttpGet]
@@ -32,18 +36,22 @@ namespace BH2KFM_HFT_2023241.Endpoint.Controllers
         public void Create([FromBody] Subject value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("SubjectCreated", value);
         }
 
         [HttpPut]
         public void Update([FromBody] Subject value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("SubjectUpdated", value);
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var value = this.logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("SubjectDeleted", value);
         }
     }
 }
