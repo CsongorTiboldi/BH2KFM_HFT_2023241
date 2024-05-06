dd875d7b4086aca19c7a27823c764cdafefd9b76
CsongorTiboldi <tiboldi.csongor@gmail.com>
2024-05-06 15:30:28 +0200

    configured CORS policy on endpoint

--- BH2KFM_HFT_2023241.Endpoint/Startup.cs
+++ BH2KFM_HFT_2023241.Endpoint/Startup.cs
@@ -69,6 +69,12 @@ namespace BH2KFM_HFT_2023241.Endpoint
                 await context.Response.WriteAsJsonAsync(response);
             }));
 
+            app.UseCors(x => x
+                .AllowCredentials()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithOrigins("http://localhost:30789"));
+
             app.UseRouting();
 
             app.UseAuthorization();
